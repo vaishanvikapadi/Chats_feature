a207785e615f041c986d329100986826a35a1dee
VaishnaviKapadi <VaishnaviKapadi7028@gmail.com>
2024-06-12 23:16:01 +0530

    errorhandleupdation

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const mongoose=require("mongoose");
 const path=require("path");
 const Chat=require("./modules/chat.js");
 const methodOverride = require('method-override');
+const ExpressError=require("./ExpressError");
 
  
 // override with the X-HTTP-Method-Override header in the request
 app.use(methodOverride('_method'))
 
-app.set("Views",path.join(__dirname,"views"));//THIS USED FOR USE THE VIEWS FOLDER
+app.set("views",path.join(__dirname,"views"));//THIS USED FOR USE THE VIEWS FOLDER
 app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname,"public")));// BY USING THIS MIDDLEWARE WE CAN ACCESS PUBLIC FOLDER FOR STATIC PART
 app.use(express.urlencoded({extended:true}));// is used to parse incoming requests with URL-encoded 
@@ -22,61 +23,84 @@ main()
 
 async function main()
 {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 //Index Route
 
-app.get("/Chats",async(req,res)=>{
-    let chats=await Chat.find();
+app.get("/Chats",async(req,res,next)=>{
+    try{
+        let chats=await Chat.find();
     // console.log(chats);
     res.render("index.ejs",{chats});
+
+    }catch(err){
+        next(err);
+    }
+    
 });
 
 
 //NEW ROUTE
 
-app.get("/chats/new",(req,res)=>{
-   res.render("new.ejs")
+app.get("/chats/new",(req,res,next)=>{
+    try{
+        res.render("new.ejs")
+    }
+    catch(err){
+        next(err);
+    }
+  
 })
 
 //create route
 
-app.post("/chats",(req,res)=>{
-    let {from,to,msg}=req.body;
-    let newChat=new Chat({
-        from: from,
-        to:to,
-        msg:msg,
-        created_at:new Date(),
-    });
-    console.log(newChat);
-    newChat
-    .save()
-    .then((res)=>{
-        console.log("chat was save");
-    })
-    .catch((err)=>
-        {
-             console.log(err)
+app.post("/chats", asyncWrap(async (req, res, next) => {
+  
+        let { from, to, msg } = req.body;
+        let newChat = new Chat({
+            from: from,
+            to: to,
+            msg: msg,
+            created_at: new Date(),
+        });
 
-         })
-    res.redirect("/chats");
-})
+        await newChat.save();
+        res.redirect("/chats");
+    
+}));
 
-//Edit ROUTES
+function asyncWrap(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err)=>next(err));
+    }
+}
+//new-show  ROUTES
 
-app.get("/chats/:id/edit",async(req,res)=>{
+app.get("/chats/:id",asyncWrap(async(req,res,next)=>{
+   
+        let { id }=req.params;
+    let chat =await Chat.findById(id);
+    if(!chat){
+           return next(new ExpressError(404, "Chats is not found"));
+        }
+        res.render("edit.ejs",{chat});
+    
+}));
+
+//Edit route 
+app.get("/chats/:id/edit",asyncWrap(async(req,res)=>{
     let {id}=req.params;
     let chat=await Chat.findById(id);
     res.render("edit.ejs",{chat});
-});
+}));
 
 //upaddet
-app.put("/chats/:id",async(req,res)=>{
-    let{id}=req.params;
+app.put("/chats/:id",asyncWrap(async(req,res)=>{
+  
+        let{id}=req.params;
     let{msg :newMsg}=req.body;
     let updatedChat=await Chat.findByIdAndUpdate(
         id,
@@ -85,16 +109,20 @@ app.put("/chats/:id",async(req,res)=>{
     );
     console.log(updatedChat);
     res.redirect("/chats");
-})
+    
+}));
 
 //Delete the chat
 
-app.delete("/chats/:id",async (req,res)=>{
-    let{id}=req.params;
-    let deleteChat=await Chat.findByIdAndDelete(id);
-    console.log(deleteChat);
-    res.redirect("/chats")
-})
+app.delete("/chats/:id",asyncWrap(async (req,res)=>{
+    
+        let{id}=req.params;
+        let deleteChat=await Chat.findByIdAndDelete(id);
+        console.log(deleteChat);
+        res.redirect("/chats")
+
+   
+}));
 
 // let chat1=new Chat({
 //     from:"neha",
@@ -113,7 +141,30 @@ app.delete("/chats/:id",async (req,res)=>{
 
 app.get("/",(req,res)=>{
     res.send("Server is working");
+});
+const handlevalidationErr=(err)=>{
+    console.log("This is Validation Error, please Follow rules ");
+    console.dir(err.message);
+    return err;
+}
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+    if(err.name=="ValidationError"){
+       handlevalidationErr(err);
+    }
+    next(err);
 })
+//404 handler
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+//Error handling middleware
+app.use((err ,req ,res,next)=>{
+    let {status=500 ,message="Some Error Occurred "}=err;
+    res.status(500).send(message);
+})
+
 app.listen(8080,()=>{
     console.log("Server is listening to the Port 8080")
 })
